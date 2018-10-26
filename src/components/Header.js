6e22801eb0cceca7cc65c3c80f2e5a2b2f97c2ee
minheinkhant <aungkoung007@gmail.com>
2018-10-26 23:21:03 +0630

    Fix typo dashnoard to dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
 
-    <NavLink activeClassName="is-active" to="/dashnoard">
+    <NavLink activeClassName="is-active" to="/dashboard">
       Dashboard
     </NavLink>
     <NavLink activeClassName="is-active" to="/create">
